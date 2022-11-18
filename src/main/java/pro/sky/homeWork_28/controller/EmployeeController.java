e75d728428ca2827dbb31cf9c6bfbd90244c33d8
Семья Ким <dell200288@yandex.ru>
2022-11-18 12:35:36 +0300

    homeWork-2.8

---
+++ src/main/java/pro/sky/homeWork_28/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWork_28.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departments")
+public class EmployeeController {
+
+
+
+}
---
+++ src/main/java/pro/sky/homeWork_28/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package pro.sky.homeWork_28.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+
+}
